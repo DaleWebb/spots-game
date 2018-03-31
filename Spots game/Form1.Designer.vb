bcc98616af8dcee37aec86058b66b2deca42c6fb
Dale Alexander Webb <dale@personajobs.com>
2018-03-31 14:32:53 +0100

    initial commit

---
+++ Spots game/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Panel2 = New System.Windows.Forms.Panel
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(18, 300)
+        Me.Panel1.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(10, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 42)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Level"
+        Me.Label1.Visible = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DarkRed
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.ShapeContainer1)
+        Me.Panel2.Location = New System.Drawing.Point(24, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(274, 269)
+        Me.Panel2.TabIndex = 5
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.OvalShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(274, 269)
+        Me.ShapeContainer1.TabIndex = 5
+        Me.ShapeContainer1.TabStop = False
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.BackColor = System.Drawing.Color.Gray
+        Me.OvalShape1.FillColor = System.Drawing.Color.Red
+        Me.OvalShape1.FillGradientColor = System.Drawing.Color.Red
+        Me.OvalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.OvalShape1.Location = New System.Drawing.Point(151, 146)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.SelectionColor = System.Drawing.Color.Transparent
+        Me.OvalShape1.Size = New System.Drawing.Size(75, 75)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkRed
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents OvalShape1 As Microsoft.VisualBasic.PowerPacks.OvalShape
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Spots game/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+    Dim Score, LevelCount, OverallScore, ClickCounter, Timeaddon As Integer
+    Dim Rand As New Random()
+    Dim Speed As Integer = 10
+    Public Sub Reset()
+        ClickCounter = 0
+        Label1.Visible = False
+        Score = 0
+        LevelCount = 0
+        OverallScore = 0
+        Speed = 10
+        OvalShape1.Location = New System.Drawing.Point(106, 12)
+        OvalShape1.Size = New System.Drawing.Point(75, 75)
+        OvalShape1.FillColor() = Color.Red
+        Panel1.Size = New System.Drawing.Point(18, Me.Size.Height / 2)
+        Me.Text = "Level: " & LevelCount / 10 & ". Score: " & OverallScore
+        MessageBox.Show("Go!", "Go!")
+        OvalShape1.Visible = True
+        Timer1.Start()
+    End Sub
+
+    Public Sub Pause()
+        Timer1.Stop()
+        Timer1.Enabled = False
+        Label1.Visible = True
+        Label1.Text = "Paused" & vbCrLf & "(click to continue)"
+        OvalShape1.Visible = False
+        Label1.Location = New System.Drawing.Point(Math.Ceiling((Me.Width / 2) - Label1.Width / 2), Math.Ceiling((Me.Height / 2) - Label1.Height / 2))
+        Panel2.Size = New System.Drawing.Point(Me.Size.Width, Me.Size.Height)
+        OvalShape1.Location = New System.Drawing.Point(Rand.Next(Convert.ToInt16(Panel2.Size.Width - (Panel2.Size.Width / 5))), Rand.Next(Convert.ToInt16(Panel2.Size.Height - (Panel2.Size.Height / 5))))
+    End Sub
+    Private Sub OvalShape1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OvalShape1.Click
+        Dim Colour As Integer
+        ClickCounter = ClickCounter + 1
+        Timer1.Start()
+        OvalShape1.Location = New System.Drawing.Point(Rand.Next(Convert.ToInt16(Panel2.Size.Width - (Panel2.Size.Width / 5))), Rand.Next(Convert.ToInt16(Panel2.Size.Height - (Panel2.Size.Height / 5))))
+        OvalShape1.Size = New System.Drawing.Point(Rand.Next(10, 100), OvalShape1.Size.Width)
+        OvalShape1.Size = New System.Drawing.Point(OvalShape1.Size.Width, OvalShape1.Size.Width)
+        Colour = Rand.Next(1, 8)
+        Select Case Colour
+            Case 1
+                OvalShape1.FillColor() = Color.Blue
+            Case 2
+                OvalShape1.FillColor() = Color.Red
+            Case 3
+                OvalShape1.FillColor() = Color.Yellow
+            Case 4
+                OvalShape1.FillColor() = Color.Green
+            Case 5
+                OvalShape1.FillColor() = Color.Orange
+            Case 6
+                OvalShape1.FillColor() = Color.Purple
+            Case 7
+                OvalShape1.FillColor() = Color.White
+            Case 8
+                OvalShape1.FillColor() = Color.Cyan
+        End Select
+
+        Select Case OvalShape1.Size.Width
+            Case 10 To 19
+                Score = 100
+            Case 20 To 29
+                Score = 90
+            Case 30 To 39
+                Score = 80
+            Case 40 To 49
+                Score = 70
+            Case 50 To 59
+                Score = 60
+            Case 60 To 69
+                Score = 50
+            Case 70 To 79
+                Score = 40
+            Case 80 To 89
+                Score = 30
+            Case 90 To 99
+                Score = 20
+            Case 100
+                Score = 10
+        End Select
+
+        Panel1.Size = New System.Drawing.Point(Panel1.Size.Width, Panel1.Size.Height + (OvalShape1.Size.Width + Score))
+        LevelCount = LevelCount + 1
+        Timeaddon = Timeaddon + (Score * (LevelCount / 100))
+        Select Case ClickCounter
+            Case 10
+                OvalShape1.Visible = False
+                Label1.Text = "Level " & Math.Ceiling(LevelCount / 10)
+                Label1.Visible = True
+                Timer1.Stop()
+                Timer1.Enabled = False
+                Speed = Speed + 2
+                Panel1.Size = New System.Drawing.Point(18, Panel1.Height + Timeaddon)
+                MessageBox.Show("Level " & Math.Ceiling(LevelCount / 10) & ", Time added (+" & Timeaddon & ")", "New Level")
+                ClickCounter = 0
+                Timeaddon = 0
+        End Select
+        If Timer1.Enabled = False Then
+            Label1.Visible = False
+            OvalShape1.Visible = True
+            Timer1.Enabled = True
+            Timer1.Start()
+        End If
+        OverallScore = OverallScore + Score
+        Me.Text = "Level: " & Math.Floor(LevelCount / 10) & ". Score: " & OverallScore
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Panel1.Size = New System.Drawing.Point(Panel1.Size.Width, Panel1.Size.Height - Speed)
+        Label2.Text = Panel1.Height
+        If Panel1.Size.Height = 0 Then
+            Timer1.Stop()
+            Label1.Text = "Game Over!"
+            Label1.Visible = True
+            OvalShape1.Visible = False
+            If MessageBox.Show("Game Over!" & vbCrLf & "Level " & Math.Floor(LevelCount / 10) & vbCrLf & "Score: " & OverallScore & vbCrLf & "Number of circles clicked: " & LevelCount & vbCrLf & "New Game?", "Game Over!", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                Reset()
+            Else
+                End
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Show()
+        Panel1.Size = New System.Drawing.Point(18, Me.Size.Height / 2)
+        Label1.Location = New System.Drawing.Point(Math.Ceiling((Panel2.Size.Width / 2) - Label1.Size.Width / 2), Math.Ceiling((Panel2.Size.Height / 2) - Label1.Size.Height / 2))
+        Reset()
+    End Sub
+
+    Private Sub Form1_Move(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Move
+        Pause()
+    End Sub
+
+    Private Sub Form1_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
+        Pause()
+    End Sub
+
+    Private Sub Panel2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Panel2.Click
+        If Timer1.Enabled = False Then
+            Label1.Visible = False
+            OvalShape1.Visible = True
+            Timer1.Enabled = True
+            Timer1.Start()
+        End If
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Label1.Click
+        If Timer1.Enabled = False Then
+            Label1.Visible = False
+            OvalShape1.Visible = True
+            Timer1.Enabled = True
+            Timer1.Start()
+        End If
+    End Sub
+End Class
